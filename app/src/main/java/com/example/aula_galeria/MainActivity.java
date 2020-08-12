6afe2c87e3ead935bde1edf5c5565186d85689c0
mbegnini <mauricio1989@gmail.com>
2020-08-12 18:16:19 -0300

    1

--- app/src/main/java/com/example/aula_galeria/MainActivity.java
+++ app/src/main/java/com/example/aula_galeria/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.aula_galeria;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-
 import android.Manifest;
 import android.content.ContentValues;
 import android.content.Intent;
@@ -13,7 +11,6 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -21,20 +18,17 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSION_CODE = 1;
     private static final int TAKE_PICTURE_CODE = 2;
+    private static final int OPEN_GALLERY_CODE = 3;
 
-    private ImageView imageViewPicture;
-    private ImageButton imageButtonCamera;
-    private ImageButton imageButtonGallery;
+    public ImageView imageViewPicture;
+    public ImageButton imageButtonCamera;
+    public  ImageButton imageButtonGallery;
 
     private String currentPatch;
     private Uri image_uri;
@@ -42,9 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        imageViewPicture = (ImageView) findViewById(R.id.imageViewPicture);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageViewPicture = (ImageView) findViewById(R.id.imageViewPicture);
     }
 
     public void openCamera(View view) {
@@ -60,15 +55,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openGallery(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, OPEN_GALLERY_CODE);
     }
 
     private void openCamera(){
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                startActivityForResult(takePictureIntent, TAKE_PICTURE_CODE);
-            } else {
-                Toast.makeText(this, "Dispositivo não suporta abrir camera em aplicações externas", Toast.LENGTH_SHORT).show();
-            }
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"Nova imagem");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"descrição");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, TAKE_PICTURE_CODE);
+        } else {
+            Toast.makeText(this, "Dispositivo não suporta abrir camera em aplicações externas", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -88,15 +93,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.d("debuggg","rc "+requestCode+"rc "+resultCode);
         if(requestCode == TAKE_PICTURE_CODE && resultCode == RESULT_OK){
-
-            Bundle extras = data.getExtras();
-            Log.d("debuggg","bundle "+extras);
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            Log.d("debuggg","bitmap "+imageBitmap);
-            imageViewPicture.setImageBitmap(imageBitmap);
-            Log.d("debuggg","erro" );
-        }
+            imageViewPicture.setImageURI(image_uri);
+        }else
+            if(requestCode == OPEN_GALLERY_CODE && resultCode == RESULT_OK){
+                image_uri = data.getData();
+                imageViewPicture.setImageURI(image_uri);
+            }
     }
 }
